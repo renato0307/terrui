1842ed6715d5687cf53d0f2f95ee5e387f428781
Renato Torres <renato.torres@outsystems.com>
2022-04-09 10:00:45 +0100

    refactor: add generic list

---
+++ internal/ui/list.go
@@ -0,0 +1,195 @@
+package ui
+
+import (
+	"fmt"
+
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
+
+type ListPageSource interface {
+	ActionSelectWorkspace(table *tview.Table, currentItem int) func(ek *tcell.EventKey) *tcell.EventKey
+
+	Crumb() []string
+	Name() string
+
+	RenderHeader(table *tview.Table)
+	RenderRows(table *tview.Table)
+	Search(searchText string, pageNumber int) error
+
+	Empty() bool
+	CurrentPage() int
+	TotalCount() int
+	TotalPages() int
+}
+
+type ListPage struct {
+	app    *App
+	source ListPageSource
+
+	*tview.Flex
+	table *tview.Table
+
+	pagination  *tview.TextView
+	searchInput *tview.InputField
+	searching   bool
+
+	currentItem int
+}
+
+func NewListPage(app *App, source ListPageSource) Page {
+	l := ListPage{
+		app:    app,
+		source: source,
+
+		Flex:        tview.NewFlex(),
+		table:       tview.NewTable(),
+		pagination:  tview.NewTextView(),
+		searchInput: tview.NewInputField(),
+
+		currentItem: 1,
+	}
+
+	headerFlex := tview.NewFlex()
+	headerFlex.AddItem(l.searchInput, 0, 1, false)
+	headerFlex.AddItem(l.pagination, 0, 1, false)
+
+	l.searchInput.SetFieldBackgroundColor(l.GetBackgroundColor())
+	l.pagination.SetTextAlign(tview.AlignRight)
+
+	l.AddItem(headerFlex, 2, 0, false).
+		SetDirection(tview.FlexRow).
+		AddItem(l.table, 0, 1, true)
+
+	return &l
+}
+
+func (l *ListPage) Load() error {
+	return l.source.Search("", -1)
+}
+
+func (l *ListPage) View() string {
+	l.table.Clear()
+	l.table.SetSelectable(true, false)
+
+	l.source.RenderHeader(l.table)
+
+	if l.source.Empty() {
+		return fmt.Sprintf("no %s found", l.source.Name())
+	}
+
+	l.source.RenderRows(l.table)
+
+	l.table.SetSelectionChangedFunc(func(row, column int) {
+		l.currentItem = row
+	})
+
+	l.pagination.SetText(
+		fmt.Sprintf("page %d of %d, total %s: %d",
+			l.source.CurrentPage(),
+			l.source.TotalPages(),
+			l.source.Name(),
+			l.source.TotalCount()))
+
+	return "workspaces loaded"
+}
+
+func (l *ListPage) BindKeys() KeyActions {
+	return KeyActions{
+		tcell.KeyEnter: NewKeyAction("select workspace", l.actionSelectWorkspace, true),
+		tcell.KeyCtrlJ: NewKeyAction("next page", l.actionPaginationNextPage, true),
+		tcell.KeyCtrlK: NewKeyAction("previous page", l.actionPaginationPrevPage, true),
+		KeySlash:       NewKeyAction("search workspaces", l.actionSearch, true),
+		tcell.KeyEsc:   NewKeyAction("cancel search if search results are active", l.actionCancelSearch, true),
+	}
+}
+
+func (l *ListPage) Crumb() []string {
+	return l.source.Crumb()
+}
+
+func (l *ListPage) Name() string {
+	return l.source.Name()
+}
+
+func (l *ListPage) Footer() string {
+	return ""
+}
+
+func (l *ListPage) actionSelectWorkspace(ek *tcell.EventKey) *tcell.EventKey {
+	if l.searching {
+		return ek
+	}
+
+	return l.source.ActionSelectWorkspace(l.table, l.currentItem)(ek)
+}
+
+func (l *ListPage) actionPaginationNextPage(ek *tcell.EventKey) *tcell.EventKey {
+	go l.app.ExecPageWithLoadFunc(l, l.loadNextPageFunc(), false)
+	return nil
+}
+
+func (l *ListPage) actionPaginationPrevPage(ek *tcell.EventKey) *tcell.EventKey {
+	go l.app.ExecPageWithLoadFunc(l, l.loadPrevPageFunc(), false)
+	return nil
+}
+
+func (l *ListPage) actionSearch(ek *tcell.EventKey) *tcell.EventKey {
+	l.searching = true
+
+	l.searchInput.SetFieldBackgroundColor(tview.Styles.ContrastBackgroundColor)
+	l.searchInput.SetLabel("🔎 ")
+	l.searchInput.SetDoneFunc(func(key tcell.Key) {
+		switch key {
+		case tcell.KeyEscape:
+			l.searchInput.SetFieldBackgroundColor(l.GetBackgroundColor())
+			l.searchInput.SetLabel("")
+			l.searchInput.SetText("")
+		case tcell.KeyEnter:
+			go l.app.ExecPageWithLoadFunc(l, l.loadSearchFunc(), false)
+		}
+		l.searching = false
+		l.app.SetFocus(l.table)
+	})
+	l.app.SetFocus(l.searchInput)
+	return nil
+}
+
+func (l *ListPage) actionCancelSearch(ek *tcell.EventKey) *tcell.EventKey {
+	if l.searchInput.GetText() == "" {
+		return ek
+	}
+
+	l.searchInput.SetFieldBackgroundColor(l.GetBackgroundColor())
+	l.searchInput.SetLabel("")
+	l.searchInput.SetText("")
+	go l.app.ExecPageWithLoadFunc(l, l.loadSearchFunc(), false)
+
+	return nil
+}
+
+func (l *ListPage) loadNextPageFunc() func() error {
+	return func() error {
+		pageToLoad := l.source.CurrentPage() + 1
+		if pageToLoad > l.source.TotalPages() {
+			pageToLoad = 1
+		}
+		return l.source.Search(l.searchInput.GetText(), pageToLoad)
+	}
+}
+
+func (l *ListPage) loadPrevPageFunc() func() error {
+	return func() error {
+		pageToLoad := l.source.CurrentPage() - 1
+		if pageToLoad < 1 {
+			pageToLoad = l.source.TotalPages()
+		}
+		return l.source.Search(l.searchInput.GetText(), pageToLoad)
+	}
+}
+
+func (l *ListPage) loadSearchFunc() func() error {
+	return func() error {
+		return l.source.Search(l.searchInput.GetText(), -1)
+	}
+}
--- internal/ui/workspaces.go
+++ internal/ui/workspaces.go
@@ -13,48 +13,16 @@ import (
 
 const WorkspacesPageName string = "workspaces"
 
-type WorkspacesPage struct {
-	*tview.Flex
-	table       *tview.Table
-	pagination  *tview.TextView
-	searchInput *tview.InputField
-
-	app              *App
-	workspaces       *tfe.WorkspaceList
-	currentWorkspace int
-
-	searching bool
+type WorkspacesPageSource struct {
+	app        *App
+	workspaces *tfe.WorkspaceList
 }
 
 func NewWorkspacesPage(app *App) Page {
-	w := WorkspacesPage{
-		Flex:             tview.NewFlex(),
-		table:            tview.NewTable(),
-		pagination:       tview.NewTextView(),
-		searchInput:      tview.NewInputField(),
-		app:              app,
-		currentWorkspace: 1,
-	}
-
-	headerFlex := tview.NewFlex()
-	headerFlex.AddItem(w.searchInput, 0, 1, false)
-	headerFlex.AddItem(w.pagination, 0, 1, false)
-
-	w.searchInput.SetFieldBackgroundColor(w.GetBackgroundColor())
-	w.pagination.SetTextAlign(tview.AlignRight)
-
-	w.AddItem(headerFlex, 2, 0, false).
-		SetDirection(tview.FlexRow).
-		AddItem(w.table, 0, 1, true)
-
-	return &w
+	return NewListPage(app, &WorkspacesPageSource{app: app})
 }
 
-func (w *WorkspacesPage) Load() error {
-	return w.load("", -1)
-}
-
-func (w *WorkspacesPage) load(searchText string, pageNumber int) error {
+func (w *WorkspacesPageSource) Search(searchText string, pageNumber int) error {
 	tfeClient, err := client.NewTFEClient()
 	if err != nil {
 		return fmt.Errorf("error creating the TFE client: %w", err)
@@ -62,51 +30,36 @@ func (w *WorkspacesPage) load(searchText string, pageNumber int) error {
 
 	workspaces, err := tfeClient.ListWorkspaces(w.app.config.Organization, searchText, pageNumber)
 	if err != nil {
-		return fmt.Errorf("error listing the organization: %w", err)
+		return fmt.Errorf("error listing the workspaces: %w", err)
 	}
 	w.workspaces = workspaces
 
 	return nil
 }
 
-func (w *WorkspacesPage) View() string {
-	w.table.Clear()
+func (w *WorkspacesPageSource) RenderHeader(table *tview.Table) {
+	table.Clear()
+	table.SetSelectable(true, false)
+	table.SetCell(0, 0, tview.NewTableCell("ID").SetSelectable(false))
+	table.SetCell(0, 1, tview.NewTableCell("NAME").SetSelectable(false))
+	table.SetCell(0, 2, tview.NewTableCell("TAGS").SetSelectable(false))
+	table.SetCell(0, 3, tview.NewTableCell("TERRAFORM").SetSelectable(false))
+	table.SetCell(0, 4, tview.NewTableCell("COUNT").SetSelectable(false))
+	table.SetCell(0, 5, tview.NewTableCell("RUN STATUS").SetSelectable(false))
+	table.SetCell(0, 5, tview.NewTableCell("LATEST CHANGE").SetSelectable(false))
+}
 
-	w.table.SetSelectable(true, false)
-	w.table.SetCell(0, 0, tview.NewTableCell("ID").SetSelectable(false))
-	w.table.SetCell(0, 1, tview.NewTableCell("NAME").SetSelectable(false))
-	w.table.SetCell(0, 2, tview.NewTableCell("TAGS").SetSelectable(false))
-	w.table.SetCell(0, 3, tview.NewTableCell("TERRAFORM").SetSelectable(false))
-	w.table.SetCell(0, 4, tview.NewTableCell("COUNT").SetSelectable(false))
-	w.table.SetCell(0, 5, tview.NewTableCell("RUN STATUS").SetSelectable(false))
-	w.table.SetCell(0, 5, tview.NewTableCell("LATEST CHANGE").SetSelectable(false))
-
-	if w.workspaces == nil || w.workspaces.TotalCount == 0 {
-		return "no workspaces found"
-	}
-
+func (w *WorkspacesPageSource) RenderRows(table *tview.Table) {
 	for i, wi := range w.workspaces.Items {
 		r := i + 1
-		w.table.SetCell(r, 0, tview.NewTableCell(wi.ID).SetExpansion(1))
-		w.table.SetCell(r, 1, tview.NewTableCell(wi.Name).SetExpansion(1))
-		w.table.SetCell(r, 2, fmtTags(wi).SetExpansion(1))
-		w.table.SetCell(r, 3, tview.NewTableCell(wi.TerraformVersion).SetExpansion(2))
-		w.table.SetCell(r, 4, tview.NewTableCell(fmt.Sprint(wi.ResourceCount)).SetExpansion(2))
-		w.table.SetCell(r, 5, fmtCurrentRun(wi).SetExpansion(1))
-		w.table.SetCell(r, 6, fmtUpdatedAt(wi).SetExpansion(1))
+		table.SetCell(r, 0, tview.NewTableCell(wi.ID).SetExpansion(1))
+		table.SetCell(r, 1, tview.NewTableCell(wi.Name).SetExpansion(1))
+		table.SetCell(r, 2, fmtTags(wi).SetExpansion(1))
+		table.SetCell(r, 3, tview.NewTableCell(wi.TerraformVersion).SetExpansion(2))
+		table.SetCell(r, 4, tview.NewTableCell(fmt.Sprint(wi.ResourceCount)).SetExpansion(2))
+		table.SetCell(r, 5, fmtCurrentRun(wi).SetExpansion(1))
+		table.SetCell(r, 6, fmtUpdatedAt(wi).SetExpansion(1))
 	}
-
-	w.table.SetSelectionChangedFunc(func(row, column int) {
-		w.currentWorkspace = row
-	})
-
-	w.pagination.SetText(
-		fmt.Sprintf("page %d of %d, total workspaces: %d",
-			w.workspaces.CurrentPage,
-			w.workspaces.TotalPages,
-			w.workspaces.TotalCount))
-
-	return "workspaces loaded"
 }
 
 func fmtTags(w *tfe.Workspace) *tview.TableCell {
@@ -142,110 +95,38 @@ func fmtCurrentRun(w *tfe.Workspace) *tview.TableCell {
 	return tview.NewTableCell(fmt.Sprint(w.CurrentRun.Status)).SetStyle(style)
 }
 
-func (w *WorkspacesPage) BindKeys() KeyActions {
-	return KeyActions{
-		tcell.KeyEnter: NewKeyAction("select workspace", w.actionSelectWorkspace, true),
-		tcell.KeyCtrlJ: NewKeyAction("next page", w.actionPaginationNextPage, true),
-		tcell.KeyCtrlK: NewKeyAction("previous page", w.actionPaginationPrevPage, true),
-		KeySlash:       NewKeyAction("search workspaces", w.actionSearch, true),
-		tcell.KeyEsc:   NewKeyAction("cancel search if search results are active", w.actionCancelSearch, true),
-	}
-}
-
-func (w *WorkspacesPage) Crumb() []string {
+func (w *WorkspacesPageSource) Crumb() []string {
 	return []string{
 		w.app.config.Organization,
 		WorkspacesPageName,
 	}
 }
 
-func (w *WorkspacesPage) Name() string {
+func (w *WorkspacesPageSource) Name() string {
 	return WorkspacesPageName
 }
 
-func (w *WorkspacesPage) Footer() string {
-	return ""
-}
-
-func (w *WorkspacesPage) actionSelectWorkspace(ek *tcell.EventKey) *tcell.EventKey {
-	if w.searching {
-		return ek
-	}
-
-	w.app.config.Workspace = w.table.GetCell(w.currentWorkspace, 1).Text
-	w.app.config.Save()
-
-	w.app.activatePage(WorkspacePageName, nil, false)
-
-	return nil
-}
-
-func (w *WorkspacesPage) actionPaginationNextPage(ek *tcell.EventKey) *tcell.EventKey {
-	go w.app.ExecPageWithLoadFunc(w, w.loadNextPageFunc(), false)
-	return nil
-}
-
-func (w *WorkspacesPage) actionPaginationPrevPage(ek *tcell.EventKey) *tcell.EventKey {
-	go w.app.ExecPageWithLoadFunc(w, w.loadPrevPageFunc(), false)
-	return nil
-}
-
-func (w *WorkspacesPage) actionSearch(ek *tcell.EventKey) *tcell.EventKey {
-	w.searching = true
-
-	w.searchInput.SetFieldBackgroundColor(tview.Styles.ContrastBackgroundColor)
-	w.searchInput.SetLabel("🔎 ")
-	w.searchInput.SetDoneFunc(func(key tcell.Key) {
-		switch key {
-		case tcell.KeyEscape:
-			w.searchInput.SetFieldBackgroundColor(w.GetBackgroundColor())
-			w.searchInput.SetLabel("")
-			w.searchInput.SetText("")
-		case tcell.KeyEnter:
-			go w.app.ExecPageWithLoadFunc(w, w.loadSearchFunc(), false)
-		}
-		w.searching = false
-		w.app.SetFocus(w.table)
-	})
-	w.app.SetFocus(w.searchInput)
-	return nil
-}
-
-func (w *WorkspacesPage) actionCancelSearch(ek *tcell.EventKey) *tcell.EventKey {
-	if w.searchInput.GetText() == "" {
-		return ek
-	}
-
-	w.searchInput.SetFieldBackgroundColor(w.GetBackgroundColor())
-	w.searchInput.SetLabel("")
-	w.searchInput.SetText("")
-	go w.app.ExecPageWithLoadFunc(w, w.loadSearchFunc(), false)
-
-	return nil
-}
-
-func (w *WorkspacesPage) loadNextPageFunc() func() error {
-	return func() error {
-		pageToLoad := w.workspaces.CurrentPage + 1
-		if pageToLoad > w.workspaces.TotalPages {
-			pageToLoad = 1
-		}
-		return w.load(w.searchInput.GetText(), pageToLoad)
+func (w *WorkspacesPageSource) ActionSelectWorkspace(table *tview.Table, currentItem int) func(ek *tcell.EventKey) *tcell.EventKey {
+	return func(ek *tcell.EventKey) *tcell.EventKey {
+		w.app.config.Workspace = table.GetCell(currentItem, 1).Text
+		w.app.config.Save()
+		w.app.activatePage(WorkspacePageName, nil, false)
+		return nil
 	}
 }
 
-func (w *WorkspacesPage) loadPrevPageFunc() func() error {
-	return func() error {
-		pageToLoad := w.workspaces.CurrentPage - 1
-		if pageToLoad < 1 {
-			pageToLoad = w.workspaces.TotalPages
-		}
-		return w.load(w.searchInput.GetText(), pageToLoad)
-	}
+func (w *WorkspacesPageSource) Empty() bool {
+	return false
 }
 
-func (w *WorkspacesPage) loadSearchFunc() func() error {
-	return func() error {
-		return w.load(w.searchInput.GetText(), -1)
-	}
+func (w *WorkspacesPageSource) CurrentPage() int {
+	return w.workspaces.CurrentPage
+}
+
+func (w *WorkspacesPageSource) TotalCount() int {
+	return w.workspaces.TotalCount
+}
+
+func (w *WorkspacesPageSource) TotalPages() int {
+	return w.workspaces.TotalPages
 }
